442b01fcc12713ade00274394a23463c4a7c7dca
Thomas Park <tpark1@wpi.edu>
2023-04-10 08:45:30 -0400

    Firebase implemented

---
+++ app/src/main/java/com/example/buzzwordsv1_1/DefinitionActivity.java
@@ -0,0 +1,14 @@
+package com.example.buzzwordsv1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DefinitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_definition);
+    }
+}
--- app/src/main/java/com/example/buzzwordsv1_1/MainActivity.java
+++ app/src/main/java/com/example/buzzwordsv1_1/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.buzzwordsv1_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToDefinition(View v) {
+        Intent tent = new Intent(this, DefinitionActivity.class);
+        startActivity(tent);
     }
 }
