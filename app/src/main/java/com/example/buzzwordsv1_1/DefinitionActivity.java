262f2e419a2ac5db52b75988554329e198f95b58
Thomas Park <tpark1@wpi.edu>
2023-04-11 12:12:03 -0400

    Finished definition page, fixed xml

--- app/src/main/java/com/example/buzzwordsv1_1/DefinitionActivity.java
+++ app/src/main/java/com/example/buzzwordsv1_1/DefinitionActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 public class DefinitionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_definition);
     }
 
