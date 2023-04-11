8cd8580ced7f0ad4bd5a938475c2356f3976a8a6
Thomas Park <tpark1@wpi.edu>
2023-04-11 12:09:02 -0400

    Finished definition page

--- app/src/main/java/com/example/buzzwordsv1_1/DefinitionActivity.java
+++ app/src/main/java/com/example/buzzwordsv1_1/DefinitionActivity.java
@@ -3,6 +3,7 @@ package com.example.buzzwordsv1_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class DefinitionActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class DefinitionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definition);
+    }
+
+    public void returnToMain(View v){
+        finish();
     }
 }
