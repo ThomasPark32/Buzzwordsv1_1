2bfcfc4fa4852ffde10e9c6c7f88c09697505c3f
Thomas Park <tpark1@wpi.edu>
2023-04-18 13:37:03 -0400

    Added contact us page

---
+++ app/src/main/java/com/example/buzzwordsv1_1/ContactUsActivity.java
@@ -0,0 +1,25 @@
+package com.example.buzzwordsv1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_contact_us);
+    }
+
+    /**
+     * Quits the current activity and goes back to the previous screen.
+     * @param v the view
+     */
+    public void returnToMain(View v){
+        finish();
+    }
+}
--- app/src/main/java/com/example/buzzwordsv1_1/MainActivity.java
+++ app/src/main/java/com/example/buzzwordsv1_1/MainActivity.java
@@ -1,13 +1,6 @@
 package com.example.buzzwordsv1_1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +11,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void goToAbout(View v) {
         Intent tent = new Intent(this, AboutActivity.class);
+        startActivity(tent);
+    }
+
+    public void goToContactUs(View v) {
+        Intent tent = new Intent(this, ContactUsActivity.class);
         startActivity(tent);
     }
 
