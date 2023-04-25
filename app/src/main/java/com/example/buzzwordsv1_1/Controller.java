de5826a8af45659a48a24e9dc6996788f156b77f
Thomas Park <tpark1@wpi.edu>
2023-04-25 14:52:17 -0400

    Added: - 3rd trending word to Main Activity - Displaying trending words to Main Activity functionality - Added comments to Controller class

--- app/src/main/java/com/example/buzzwordsv1_1/Controller.java
+++ app/src/main/java/com/example/buzzwordsv1_1/Controller.java
@@ -4,26 +4,67 @@ import android.app.Application;
 
 import java.util.ArrayList;
 
+/**
+ * This class is a controller object which stores the Buzzwords in the app for usage in multiple activities.
+ */
 public class Controller extends Application {
     ArrayList<Buzzword> buzzwords = new ArrayList<Buzzword>();
+    Buzzword wotd;
 
+    /**
+     * Returns the ArrayList of Buzzwords obtained from Firebase.
+     * @return the ArrayList of Buzzword objects
+     */
     public ArrayList<Buzzword> getBuzzwords(){
         return buzzwords;
     }
 
+    /**
+     * Adds a Buzzword object to the ArrayList of Buzzword objects.
+     * @param word the Buzzword to be added
+     */
     public void addBuzzwords(Buzzword word) {
         buzzwords.add(word);
     }
 
+    /**
+     * Returns the Buzzword stored at the provided index in the ArrayList of Buzzword objects.
+     * @param index the index where the Buzzword is located
+     * @return the Buzzword stored in that index in the ArrayList of Buzzword objects
+     */
     public Buzzword getBuzzword(int index){
         return buzzwords.get(index);
     }
 
+    /**
+     * Removes a Buzzword object from the ArrayList of Buzzword objects.
+     * @param word the Buzzword object to be removed.
+     */
     public void removeBuzzword(Buzzword word) {
         buzzwords.remove(word);
     }
 
+    /**
+     * Removes a Buzzword object from the ArrayList of Buzzword objects, using an index.
+     * @param index the index where the Buzzword object is stored in the ArrayList of Buzzword objects
+     */
     public void removeBuzzword(int index) {
         buzzwords.remove(index);
+    }
+
+    /**
+     * Sets the word of the day to a given Buzzword object.
+     * @param word a Buzzword
+     */
+    public void setWOTD(Buzzword word){
+        wotd = word;
+    }
+
+    /**
+     * Returns the Buzzword object that is the word of the day.
+     * @return the word of the day, expressed as a Buzzword
+     */
+    public Buzzword getWOTD(){
+        return wotd;
     }
 }
--- app/src/main/java/com/example/buzzwordsv1_1/MainActivity.java
+++ app/src/main/java/com/example/buzzwordsv1_1/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,8 +17,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    // Determines if the Buzzword list has been updated (i.e. if it is a new day)
+    boolean newDay = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
      * @param v the view
      */
     public void goToDefinition(View v) {
+        // Global controller class object
+        final Controller aController = (Controller) getApplicationContext();
+        int length = aController.getBuzzwords().size();
+        Log.d("What", "Size of Buzzwords list: " + length);
         Intent tent = new Intent(this, DefinitionActivity.class);
         startActivity(tent);
     }
@@ -87,34 +95,43 @@ public class MainActivity extends AppCompatActivity {
      * Reads from Firebase Realtime Storage to obtain Buzzwords and populate Controller class.
      */
     private void readBuzzwordDataFB(){
-        // Global controller class object
-        final Controller aController = (Controller) getApplicationContext();
-
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("Trending");
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
+            // Global controller class object
+            final Controller aController = (Controller) getApplicationContext();
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                for (int i = 0; i < dataSnapshot.getChildrenCount(); i++) {
-                    String bruh = dataSnapshot.child("/" + i + "/word").getValue().toString();
-                    Log.d("KeepItUpBaby", "Buzzword: " + bruh);
-                    ArrayList<String> ding = (ArrayList<String>) dataSnapshot.child("/"+ i +"/definitions").getValue();
-                    Buzzword buddy = new Buzzword(bruh);
-                    if (ding != null) {
-                        for (int k = 0; k < ding.size(); k++) {
-                            Log.d("KeepItUpBaby", "Definition: " + ding.get(k));
-                            buddy.addDefinition(ding.get(k));
+                // This method is called once with the initial value and againnwhenever data at this location is updated.
+                // Loop over all children in the trending tab
+                for (int index = 0; index < dataSnapshot.getChildrenCount(); index++) {
+                    // Get buzzword Firebase word
+                    String str = dataSnapshot.child("/" + index + "/word").getValue().toString();
+                    Log.d("KeepItUpBaby", "Buzzword: " + str);
+                    // Get ArrayList from Firebase definitions
+                    ArrayList<String> definitions = (ArrayList<String>) dataSnapshot.child("/"+ index +"/definitions").getValue();
+                    // Make new buzzword object with word
+                    Buzzword word = new Buzzword(str);
+                    // Add definitions to buzzword
+                    if (definitions != null) {
+                        for (int k = 0; k < definitions.size(); k++) {
+                            Log.d("KeepItUpBaby", "Definition: " + definitions.get(k));
+                            word.addDefinition(definitions.get(k));
                         }
                     }
-                    aController.addBuzzwords(buddy);
+                    // Add buzzword object to global Controller object
+                    aController.addBuzzwords(word);
                 }
-                for (Buzzword buzzy : aController.getBuzzwords()) {
-                    Log.d("KeepItUpBaby", "Buzzword ToString: " + buzzy.toString());
+                // Choose Word of the Day, if it is a new day
+                if (newDay == true) {
+                    chooseWOTD(aController.getBuzzwords().size());
                 }
+                // Display buzzwords to main activity screen
+                sendBuzzwords();
+                // Set the newDay boolean to false
+                newDay = false;
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
@@ -122,5 +139,71 @@ public class MainActivity extends AppCompatActivity {
                 Log.w("KeepItUpBaby", "Failed to read value.", error.toException());
             }
         });
+    }
+    /**
+     * Sends top 3 trending buzzwords to UI to be displayed
+     */
+    public void sendBuzzwords(){
+        // Global controller class object
+        final Controller aController = (Controller) getApplicationContext();
+        // Get first 3 trending buzzword objects from global Controller object
+        Buzzword trending1 = aController.getBuzzword(0);
+        Buzzword trending2 = aController.getBuzzword(1);
+        Buzzword trending3 = aController.getBuzzword(2);
+        // Set titles on main activity with first 3 trending words
+        TextView title1 = findViewById(R.id.TrendingWord1Txt);
+        title1.setText(capitalized(trending1.getBuzzword()));
+        TextView title2 = findViewById(R.id.TrendingWord2Txt);
+        title2.setText(capitalized(trending2.getBuzzword()));
+        TextView title3 = findViewById(R.id.TrendingWord3Txt);
+        title3.setText(capitalized(trending3.getBuzzword()));
+        // Set definitions on main activity with first 3 trending definitions
+        TextView box1 = findViewById(R.id.TrendingWord1Def);
+        box1.setText(checkMultipleDefinitions(trending1));
+        TextView box2 = findViewById(R.id.TrendingWord2Def);
+        box2.setText(checkMultipleDefinitions(trending2));
+        TextView box3 = findViewById(R.id.TrendingWord3Def);
+        box3.setText(checkMultipleDefinitions(trending3));
+        // Set Word of the Day text boxes
+        Buzzword wotd = aController.getWOTD();
+        TextView wotdTitle = findViewById(R.id.wotdTxt);
+        wotdTitle.setText(capitalized(wotd.getBuzzword()));
+        TextView wotdDef = findViewById(R.id.wotdDef);
+        wotdDef.setText(checkMultipleDefinitions(wotd));
+    }
+    /**
+     * Returns a string with the first character capitalized.
+     * @param str the string to be capitalized
+     * @return the capitalized string
+     */
+    private String capitalized(String str){
+        String capital = str.substring(0,1).toUpperCase() + str.substring(1);
+        return capital;
+    }
+    /**
+     * Checks if a Buzzword object has more than one object. If it does, then it will display that there are multiple definitions to the Buzzword.
+     * @param buzz the Buzzword object to be checked
+     * @return the single definition of the Buzzword or the string "Multiple definitions available." if there is more than one definition for the Buzzword
+     */
+    private String checkMultipleDefinitions(Buzzword buzz){
+        String stringy = "";
+        if (buzz.getDefinitions().size() == 0) {
+            stringy = "No definitions available, please contact us!";
+        } else if (buzz.getDefinitions().size() < 2) {
+            stringy = buzz.getDefinitions().get(0) +".";
+        } else {
+            stringy = "Multiple definitions available.";
+        }
+        return stringy;
+    }
+    public void chooseWOTD(int amount) {
+        // Need to choose between these words in the ArrayList
+        int checkThrough = amount - 3;
+        Random randall = new Random();
+        int choice = randall.nextInt(checkThrough) + 3;
+        // Global controller class object
+        final Controller aController = (Controller) getApplicationContext();
+        Buzzword wotd = aController.getBuzzword(choice);
+        aController.setWOTD(wotd);
     }
 }
